259aae6278e14c6f84089424efe8a8033f5d64b2
karolsw2 <karol.sw3@gmail.com>
2018-05-24 22:32:31 +0200

    Attach the login task to the login button

--- app/src/main/java/com/example/dellxps13/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/dellxps13/myapplication/MainActivity.kt
@@ -2,29 +2,42 @@ package com.example.dellxps13.myapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import android.support.design.widget.TextInputEditText
+import android.support.v7.widget.AppCompatButton
 import android.widget.Toast
 import com.example.dellxps13.myapplication.LoginTask.AsyncResponse
+import org.json.JSONObject
 
 
 class MainActivity : AppCompatActivity() {
+
+    val context = this
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val login = "admin"
-        val password = "e3FnfuVjy"
+        val loginButton = findViewById<AppCompatButton>(R.id.loginButton)
 
-        val textOutput = findViewById<TextView>(R.id.textOutput)
+        loginButton.setOnClickListener {
+            var login = findViewById<TextInputEditText>(R.id.login).text.toString()
+            var password = findViewById<TextInputEditText>(R.id.password).text.toString()
 
-        LoginTask(object : AsyncResponse {
+            LoginTask(object : AsyncResponse {
 
-            override fun processFinish(output: String) {
-                textOutput.text = output
-            }
+                override fun processFinish(output: String) {
+                    val jsonObj = JSONObject(output.substring(output.indexOf("{"), output.lastIndexOf("}") + 1))
 
-        }).execute(login, password)
+                    if (jsonObj.getBoolean("success")) {
+                        Toast.makeText(context, "Pomyślnie zalogowano do systemu", Toast.LENGTH_LONG).show()
+                    } else {
+                        Toast.makeText(context, "Niepoprawny login lub hasło", Toast.LENGTH_LONG).show()
+                    }
+
+                }
+
+            }).execute(login, password)
+        }
 
     }
 

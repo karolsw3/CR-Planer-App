8c96a86e8f352ef0f918dbd4cd3f14315f4c200a
karolsw2 <karol.sw3@gmail.com>
2018-05-26 12:51:04 +0200

    Sort the data by date

--- app/src/main/java/com/example/dellxps13/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/dellxps13/myapplication/MainActivity.kt
@@ -13,6 +13,9 @@ import android.support.constraint.ConstraintLayout
 import android.support.v7.widget.CardView
 import android.widget.LinearLayout
 import android.widget.TextView
+import org.json.JSONException
+import org.json.JSONArray
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -45,13 +48,54 @@ class MainActivity : AppCompatActivity() {
                         val jsonObj = JSONObject(output.substring(output.indexOf("{"), output.lastIndexOf("}") + 1))
                         val clients = jsonObj.getJSONArray("clients")
                         val scroll = findViewById<LinearLayout>(R.id.scroll)
+
+                        val sortedJsonArray = JSONArray()
+
+                        val jsonValues = ArrayList<JSONObject>()
                         for (i in 0 until clients.length()) {
+                            jsonValues.add(clients.getJSONObject(i))
+                        }
+                        Collections.sort(jsonValues, object : Comparator<JSONObject> {
+                            //You can change "Name" with "ID" if you want to sort by ID
+                            private val KEY_NAME = "day"
+
+                            override fun compare(a: JSONObject, b: JSONObject): Int {
+                                var valA = 0
+                                var valB = 0
+
+                                try {
+                                    valA = a.get(KEY_NAME) as Int
+                                    valB = b.get(KEY_NAME) as Int
+                                } catch (e: JSONException) {
+                                    //do something
+                                }
+
+                                return valA.compareTo(valB)
+                                //if you want to change the sort order, simply use the following:
+                                //return -valA.compareTo(valB);
+                            }
+                        })
+
+                        for (i in 0 until clients.length()) {
+                            sortedJsonArray.put(jsonValues[i])
+                        }
+
+                        for (i in 0 until sortedJsonArray.length()) {
                             val card = CardView(context)
-                            val rec = clients.getJSONObject(i)
+                            val rec = sortedJsonArray.getJSONObject(i)
                             val text = TextView(context)
-                            text.text = rec.getString("symbol")
+                            text.text = "Symbol: " + rec.getString("symbol") + " Data: " + rec.getString("date") + " (" + rec.getString("opiekun") + ")"
                             card.setCardBackgroundColor(0xff5555)
-                            card.setContentPadding(20, 10, 20, 10)
+                            card.setContentPadding(40, 20, 40, 20)
+                            card.setPadding(0, 50, 0 ,0)
+
+                            val params = LinearLayout.LayoutParams(
+                                    LinearLayout.LayoutParams.MATCH_PARENT,
+                                    LinearLayout.LayoutParams.WRAP_CONTENT
+                            )
+                            params.setMargins(0, 20, 0, 20)
+                            card.layoutParams = params
+
                             card.radius = 4F
                             card.addView(text)
                             scroll.addView(card)
